7178e787aed9c11a7abeb1646c788e1b7488a617
Jared Wilkerson <wilkerson.jared@gmail.com>
2013-10-26 17:07:39 -0400

    Add tests
    Moving on to writing RecipeActivity with tests.

---
+++ test/src/com/lyrch/openbrew/test/RecipeActivityTest.java
@@ -0,0 +1,19 @@
+package com.lyrch.openbrew.test;
+
+import com.lyrch.openbrew.RecipeActivity;
+
+import android.test.ActivityUnitTestCase;
+
+public class RecipeActivityTest extends ActivityUnitTestCase<RecipeActivity> {
+
+	public RecipeActivityTest(String name) {
+		super(RecipeActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+	}
+
+//	protected void tearDown() throws Exception {
+//	}
+
+}
