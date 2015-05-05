40b5fab51ca7e632ffd0d9a7488fc3127846a296
Jared Wilkerson <wilkerson.jared@gmail.com>
2015-05-04 20:33:40 -0400

    Build the Ingredient ListView.

--- app/src/main/java/com/lyrch/openbrew/IngredientActivity.java
+++ app/src/main/java/com/lyrch/openbrew/IngredientActivity.java
@@ -4,7 +4,14 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 
 public class IngredientActivity extends ListActivity {
@@ -19,9 +26,36 @@ public class IngredientActivity extends ListActivity {
         Resources resources = context.getResources();
         ingredients = resources.getStringArray(R.array.ingredient_types);
 
-        ArrayAdapter<String> ingredientsAdapter = new ArrayAdapter<String>(this, R.layout.ingredient_list_layout, ingredients);
+        setAdapter();
+    }
 
-        setListAdapter(ingredientsAdapter);
+    private void setAdapter() {
+        ListAdapter adapter = new BaseAdapter() {
+            public int getCount() {
+                return ingredients.length;
+            }
+
+            public Object getItem(int position) {
+                return ingredients[position];
+            }
+
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater ingredientInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View view = ingredientInflater.inflate(R.layout.list_row, null);
+                TextView ingredientTextView = (TextView) view.findViewById(R.id.TextView01);
+                ingredientTextView.setText(ingredients[position]);
+                ingredientTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+                ingredientTextView.setGravity(Gravity.CENTER);
+                ingredientTextView.setTextIsSelectable(false);
+                return view;
+            }
+        };
+        setListAdapter(adapter);
     }
 
 }
